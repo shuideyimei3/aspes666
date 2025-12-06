eba872a3e760c59a4148ef5bd9a31b51a17bbc15
shuideyimei3 <shuideyimei@gmail.com>
2025-12-06 17:02:44 +0800

    update

--- src/main/java/cn/aspes/agri/trade/mapper/PaymentRecordMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.aspes.agri.trade.mapper;
-
-import cn.aspes.agri.trade.entity.PaymentRecord;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PaymentRecordMapper extends BaseMapper<PaymentRecord> {
-}
--- src/main/java/cn/aspes/agri/trade/mapper/PurchaseDemandMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.aspes.agri.trade.mapper;
-
-import cn.aspes.agri.trade.entity.PurchaseDemand;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PurchaseDemandMapper extends BaseMapper<PurchaseDemand> {
-}
